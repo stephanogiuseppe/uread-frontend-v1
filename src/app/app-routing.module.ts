964d6c7430bc0bae32f44f253272bb6ca5a72b56
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-15 02:26:28 -0300

    Lazy load routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { AuthGuardService } from './guards/auth-guard.service';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'home',
+    loadChildren: './pages/home/home.module#HomeModule',
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+    canActivate: [AuthGuardService]
+  },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
