e1396ae73341bcfb3f3dd595d4cdab8169bed3a7
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-15 02:25:54 -0300

    Home component

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from './../../shared/modules/shared.module';
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [SharedModule, HomeRoutingModule]
+})
+export class HomeModule {}
