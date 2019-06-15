c45308682dbdc6ee854110b26b0bb02a11f3046d
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-15 02:28:37 -0300

    Add lazy route, translate in app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'uread-frontend';
+export class AppComponent implements OnInit {
+  public isAuthenticated: boolean;
+  public title = 'uread-frontend';
+
+  constructor(private userService: UserService) {}
+
+  public ngOnInit(): void {
+    this.userService.verifyDataStorage();
+    this.verifyUserAuthenticated();
+  }
+
+  private verifyUserAuthenticated(): void {
+    this.userService.userIsAuthenticated.subscribe(isAuthenticated => {
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
+import { NavBarModule } from './layouts/nav-bar/nav-bar.module';
+import { LoginModule } from './pages/login/login.module';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production
-    })
+    }),
+    NavBarModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
