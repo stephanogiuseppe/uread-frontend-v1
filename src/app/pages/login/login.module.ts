b38842fb1bb2cc4c58424a6880470a72dc275b35
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-15 14:34:31 -0300

    Initial mock template of login page

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './../../shared/modules/shared.module';
+import { MaterialModule } from './../../shared/modules/material.module';
 import { LoginComponent } from './login.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [LoginComponent, SignInComponent, SignUpComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, MaterialModule],
   exports: [LoginComponent]
 })
 export class LoginModule {}
--- src/app/pages/login/sign-in/sign-in.component.ts
+++ src/app/pages/login/sign-in/sign-in.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-  constructor() {}
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('pt-br');
+    translate.use('pt-br');
+  }
 
   public ngOnInit(): void {}
 }
