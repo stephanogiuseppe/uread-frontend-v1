c01281f94cd61e0ef6e7e0a56df027c3d36ea1c4
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-16 15:16:22 -0300

    Change sign in and sign up in login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,10 +8,16 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public isSignIn = true;
+
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('pt-br');
     translate.use('pt-br');
   }
 
   public ngOnInit(): void {}
+
+  public changeFormSignIn(): void {
+    this.isSignIn = !this.isSignIn;
+  }
 }
